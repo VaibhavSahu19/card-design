29171e14528eec661ed6df149cf221ea98e57c92
Vaibhav Sahu <vaibhavsahu200@gmail.com>
2024-12-27 12:17:36 +0530

    added more designs

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -32,7 +32,7 @@ function Card() {
 
 
     return (
-        <div className="flex justify-center items-center gap-6 p-4 bg-gradient-to-r from-blue-50 to-gray-50 shadow-lg rounded-lg w-full max-w-sm border border-gray-300 hover:shadow-2xl hover:scale-105 transition-transform duration-300 ease-in-out">
+        <div className="flex justify-center items-center gap-6 p-4 bg-gradient-to-r from-blue-50 to-gray-50 shadow-lg rounded-lg w-full max-w-sm  hover:shadow-2xl hover:scale-105 transition-transform duration-300 ease-in-out border-4 border-[black]">
             <div className="relative p-1 border-4 border-black rounded-md bg-gradient-to-b from-gray-200 to-white">
                 <img
                     className="w-24 h-24 object-cover rounded-sm"
@@ -44,14 +44,21 @@ function Card() {
                 <div className="font-semibold text-xl text-indigo-600">
                     Name: <span className="text-gray-900">{finalData.name.title + " " + finalData.name.first + " " + finalData.name.last}</span>
                 </div>
-                <div className="text-sm text-gray-700">
-                    <span className="font-medium text-purple-700">Gender:</span> {finalData.gender}
+                <div className="text-sm text-gray-700 flex items-center gap-2">
+                    <span className="font-medium text-purple-700">Gender:</span>
+                    <span className="px-2 py-1 bg-purple-100 text-purple-700 rounded-full shadow-sm border border-purple-300">
+                        {finalData.gender}
+                    </span>
                 </div>
-                <div className="text-sm text-gray-700">
-                    <span className="font-medium text-purple-700">Phone:</span> {finalData.phone}
+                <div className="text-sm text-gray-700 flex items-center gap-2">
+                    <span className="font-medium text-purple-700">Phone:</span>
+                    <span className="px-2 py-1 bg-green-100 text-green-700 rounded-lg shadow-sm border border-green-300">
+                        {finalData.phone}
+                    </span>
                 </div>
             </div>
         </div>
+
     )
 }
 
